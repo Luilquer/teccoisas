6401935c4efacdf547d19dcf0197e11eb2916df0
Carlos Luilquer Almeida Santos <luilquera@gmail.com>
2021-07-14 18:04:39 -0300

    criando a pagina do admin e o template

--- index.php
+++ index.php
@@ -4,14 +4,24 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+//rota para a pagina home
 $app->get('/', function () {
 
 	$page = new Page();
+
+	$page->setTpl("index");
+});
+
+//rota para a pagina do admin
+$app->get('/admin', function () {
+
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 });
